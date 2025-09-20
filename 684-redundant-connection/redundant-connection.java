0f98b0e8599d612bf0ff6a00e6da4c14dbd99752
Aniketgurjar <124706912+Aniketgurjar@users.noreply.github.com>
2025-09-21 00:59:08 +0530

    Time: 1 ms (69.16%) | Memory: 43 MB (88.22%) - LeetSync

---
+++ 684-redundant-connection/redundant-connection.java
@@ -0,0 +1,54 @@
+class DSU {
+    int[] parent;
+    int[] rank;
+
+    public DSU(int n) {
+        parent = new int[n];
+        rank   = new int[n];
+        for (int i =1; i < n; i++) {
+            parent[i] = i;
+            rank[i] = 0;
+        }
+    }
+
+    public int find(int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+
+    public void union(int x, int y) {
+        int xParent = find(x);
+        int yParent = find(y);
+
+        if (xParent == yParent) return;
+
+        if (rank[xParent] > rank[yParent]) {
+            parent[yParent] = xParent;
+        } else if (rank[xParent] < rank[yParent]) {
+            parent[xParent] = yParent;
+        } else {
+            parent[xParent] = yParent;
+            rank[yParent]++;
+        }
+    }
+}
+class Solution {
+    public int[] findRedundantConnection(int[][] edges) {
+        int l=edges.length+1;
+        int []ans=new int[2];
+DSU dsu=new DSU(l);
+for(int[]arr:edges){
+    if(dsu.find(arr[0])==dsu.find(arr[1])){
+ans[0]=arr[0];
+ans[1]=arr[1];
+break;
+}else{
+    dsu.union(arr[0],arr[1]);
+}
+}
+return ans;
+        
+    }
+}
